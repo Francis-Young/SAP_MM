3681741757fe3d20d77b049c9c7519968477e89f
unknown <tj161751402@163.com>
2020-08-20 18:49:35 +0800

    库存部分搜索完善

--- MM/src/mm/dao/GoodsreceiptDao.java
+++ MM/src/mm/dao/GoodsreceiptDao.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.json.*;
 
 import mm.bean.GoodsReceipt;
+import mm.bean.Material;
 import mm.bean.Order;
 import mm.bean.Requisition;
 import mm.bean.Vendor;
@@ -54,23 +55,18 @@ public class GoodsreceiptDao {
 			String sql = "" + "SELECT * FROM `Order`";
 			int flag=0;
 			
-			if(!od.getVendor_code().equals("")){
+			if(!od.getVendor_code().equals("xx")){
 				sql+=" WHERE vendor_code="+'"'+od.getVendor_code()+'"';
 				flag=1;
 			}
-			if(!od.getPur_org().equals("")){
+			if(!od.getPur_org().equals("xx")){
 				if(flag==1){
 					sql+="AND ";
 				}else{sql+="WHERE ";}
 				sql+="pur_org="+'"'+od.getPur_org()+'"';	
 				flag=1;
 			}
-			if(!od.getPur_group().equals("")){
-				if(flag==1){
-					sql+="AND ";
-				}else{sql+="WHERE ";}
-				sql+="pur_group="+'"'+od.getPur_group()+'"';
-			}
+
 			
 			PreparedStatement psmt = conn.prepareStatement(sql);
 			// 执行查询语句
@@ -95,4 +91,50 @@ public class GoodsreceiptDao {
 
 		return odlist;
 	}
+	public static ArrayList<Material> findMaterialByAnything(Material mat) {
+		ArrayList<Material> matlist = new ArrayList<Material>();
+		// 建立数据库连接
+		Connection conn = DBUtil.getConnection();
+		
+		try {
+			String sql = "" + "SELECT * FROM `Material`";
+			int flag=0;
+			
+			if(!mat.getMaterial_discr().equals("xx")){
+				sql+=" WHERE material_discr LIKE '%"+mat.getMaterial_discr()+"%'";
+				flag=1;
+			}
+			if(!mat.getMaterial_salesorg().equals("xx")){
+				if(flag==1){
+					sql+="AND ";
+				}else{sql+="WHERE ";}
+				sql+="material_salesorg="+'"'+mat.getMaterial_salesorg()+'"';	
+				flag=1;
+			}
+			
+			PreparedStatement psmt = conn.prepareStatement(sql);
+			// 执行查询语句
+			ResultSet rs = psmt.executeQuery();
+			while (rs.next()) {
+				Material mat1 = new Material();
+				
+				mat1.setMaterial_num(rs.getString("material_num"));
+				mat1.setMaterial_unitprice(rs.getString("material_unitprice"));
+				mat1.setMaterial_discr(rs.getString("material_discr"));
+				mat1.setMaterial_salesorg(rs.getString("material_salesorg"));
+				matlist.add(mat1);
+
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (NullPointerException f) {
+			f.printStackTrace();
+		} finally {
+			DBUtil.closeConnection(conn);
+		}
+
+		return matlist;
+	}
+	
 }
